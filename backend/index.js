a91068d30e475e0171a1ff080757a210639629db
venu y <129159938+venu-webdev@users.noreply.github.com>
2024-04-21 12:36:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.use(cors({
-  origin: '*',
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}))
+app.use(cors())
 
 //connecting to mongoDb Cloud
 db.connectDB()
